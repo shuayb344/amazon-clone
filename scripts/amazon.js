a0073eccfc943ecb03d7e68eeca696906bf35ffd
Shuayb Mohammed <shuayb.mohammed@astu.edu.et>
2026-01-17 10:23:16 +0300

    make cart quantity icon interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -73,7 +73,11 @@ cart.forEach((item)=>{
   }
 );
   }
+  let cartQuantity = 0;
+  cart.forEach((item)=>{
+    cartQuantity += item.quantity;
+  });
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 
-console.log(cart);
 })
 })
