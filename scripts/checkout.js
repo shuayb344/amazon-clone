8d3ffd2c1e00bfa0715b11a2af9e5dc416bc4560
Shuayb Mohammed <shuayb.mohammed@astu.edu.et>
2026-01-29 14:28:04 +0300

    add  backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,8 @@
 import { renderCheckoutPage } from "./checkout/orderSummery.js";
 import { renderPaymentSummary } from "./checkout/payment.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
+import '../data/backendpractice.js';
+
 renderCheckoutPage();
 renderPaymentSummary();
 renderCheckoutHeader();
