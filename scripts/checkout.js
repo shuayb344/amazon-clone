28ddc3a2da97e5097e1d59eb384d6d4c89d672d1
Shuayb Mohammed <shuayb.mohammed@astu.edu.et>
2026-01-31 07:02:46 +0300

    convert callbacks to promise

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,8 +2,18 @@ import { renderCheckoutPage } from "./checkout/orderSummery.js";
 import { renderPaymentSummary } from "./checkout/payment.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadProducts } from "../data/products.js";
-loadProducts(()=>{
-  renderCheckoutPage();
-  renderPaymentSummary();
-  renderCheckoutHeader();
+
+new Promise((resolve)=>{
+  loadProducts(()=>{
+     resolve()
+  });
+}).then(()=>{
+  renderCheckoutPage()
+  renderPaymentSummary()
+  renderCheckoutHeader()
 })
+  
+
+
+
+
