b31ba6f898b5875a16a3df340895711872b9b8b0
Shuayb Mohammed <shuayb.mohammed@astu.edu.et>
2026-01-31 07:47:16 +0300

    convert from promises iinto async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,13 +2,14 @@ import { renderCheckoutPage } from "./checkout/orderSummery.js";
 import { renderPaymentSummary } from "./checkout/payment.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadProductFetch } from "../data/products.js";
-
-loadProductFetch()
-.then(()=>{
+async function loadPage(){
+  await loadProductFetch()
   renderCheckoutPage()
   renderPaymentSummary()
   renderCheckoutHeader()
-})
+}
+loadPage()
+
   
 
 
