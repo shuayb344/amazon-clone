7dcbf9e8d1533a2499b279e93eec2dfba0a892c4
Shuayb Mohammed <shuayb.mohammed@astu.edu.et>
2026-01-31 07:31:03 +0300

    convert loadProducts from XMLHttpRequest to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,13 +1,10 @@
 import { renderCheckoutPage } from "./checkout/orderSummery.js";
 import { renderPaymentSummary } from "./checkout/payment.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-import { loadProducts } from "../data/products.js";
+import { loadProductFetch } from "../data/products.js";
 
-new Promise((resolve)=>{
-  loadProducts(()=>{
-     resolve()
-  });
-}).then(()=>{
+loadProductFetch()
+.then(()=>{
   renderCheckoutPage()
   renderPaymentSummary()
   renderCheckoutHeader()
